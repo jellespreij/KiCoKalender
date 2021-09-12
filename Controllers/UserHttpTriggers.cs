b0d7246c837bfeda61f59596e542bb8c02aa6469
Kim <565459@student.inholland.nl>
2021-09-12 12:37:29 +0200

    First setup

---
+++ Controllers/UserHttpTriggers.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using KiCoKalender.Models;
+using Microsoft.OpenApi.Models;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using Newtonsoft.Json;
+
+
+namespace KiCoKalender.Controllers
+{
+    public class UserHttpTriggers
+    {
+        ILogger Logger { get; }
+
+        public UserHttpTriggers(ILogger<UserHttpTriggers> Logger)
+        {
+            this.Logger = Logger;
+        }
+
+		[Function(nameof(UserHttpTriggers.GetUserById))]
+		[OpenApiOperation(operationId: "GetUserById", tags: new[] { "user" }, Summary = "Find user by ID", Description = "Returns a user.", Visibility = OpenApiVisibilityType.Important)]
+		//[OpenApiSecurity("petstore_auth", SecuritySchemeType.Http, In = OpenApiSecurityLocationType.Header, Scheme = OpenApiSecuritySchemeType.Bearer, BearerFormat = "JWT")]
+		[OpenApiParameter(name: "userId", In = ParameterLocation.Path, Required = true, Type = typeof(long), Summary = "ID of user to return", Description = "ID of user to return", Visibility = OpenApiVisibilityType.Important)]
+		[OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(User), Summary = "successful operation", Description = "successful operation", Example = typeof(DummyUserExample))]
+		[OpenApiResponseWithoutBody(statusCode: HttpStatusCode.BadRequest, Summary = "Invalid ID supplied", Description = "Invalid ID supplied")]
+		[OpenApiResponseWithoutBody(statusCode: HttpStatusCode.NotFound, Summary = "User not found", Description = "User not found")]
+		public async Task<HttpResponseData> GetUserById([HttpTrigger(AuthorizationLevel.Function, "GET", Route = "user/{userId}")] HttpRequestData req, long userId, FunctionContext executionContext)
+		{
+			// Generate output
+			HttpResponseData response = req.CreateResponse(HttpStatusCode.OK);
+
+			User user = new User();
+
+			await response.WriteAsJsonAsync(user);
+
+			return response;
+		}
+
+		[Function(nameof(UserHttpTriggers.FindByRole))]
+		[OpenApiOperation(operationId: "findUserByRole", tags: new[] { "user" }, Summary = "Finds Users by Role", Description = "Multiple status values can be provided with comma separated strings.", Visibility = OpenApiVisibilityType.Important)]
+		//[OpenApiSecurity("petstore_auth", SecuritySchemeType.Http, In = OpenApiSecurityLocationType.Header, Scheme = OpenApiSecuritySchemeType.Bearer, BearerFormat = "JWT")]
+		[OpenApiParameter(name: "role", In = ParameterLocation.Query, Required = true, Type = typeof(List<Role>), Summary = "Role value", Description = "Status values that need to be considered for filter", Visibility = OpenApiVisibilityType.Important)]
+		[OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(List<Role>), Summary = "successful operation", Description = "successful operation", Example = typeof(DummyUserExample))]
+		[OpenApiResponseWithoutBody(statusCode: HttpStatusCode.BadRequest, Summary = "Invalid role value", Description = "Invalid role value")]
+		public async Task<HttpResponseData> FindByRole([HttpTrigger(AuthorizationLevel.Function, "GET", Route = "user/FindByRole")] HttpRequestData req, FunctionContext executionContext)
+		{
+			// Generate output
+			HttpResponseData response = req.CreateResponse(HttpStatusCode.OK);
+
+			List<User> user = new List<User>() { };
+
+			await response.WriteAsJsonAsync(user);
+
+			return response;
+		}
+
+		[Function(nameof(UserHttpTriggers.AddUser))]
+		[OpenApiOperation(operationId: "addUser", tags: new[] { "user" }, Summary = "Add a user to the KiCoKalender", Description = "This adds a new user to the KiCoKalender.", Visibility = OpenApiVisibilityType.Important)]
+		//[OpenApiSecurity("petstore_auth", SecuritySchemeType.Http, In = OpenApiSecurityLocationType.Header, Scheme = OpenApiSecuritySchemeType.Bearer, BearerFormat = "JWT")]
+		[OpenApiRequestBody(contentType: "application/json", bodyType: typeof(User), Required = true, Description = "User object that needs to be added to the KiCoKalender")]
+		[OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(User), Summary = "New user added", Description = "New user added", Example = typeof(DummyUserExample))]
+		[OpenApiResponseWithoutBody(statusCode: HttpStatusCode.MethodNotAllowed, Summary = "Invalid input", Description = "Invalid input")]
+		public async Task<HttpResponseData> AddUser([HttpTrigger(AuthorizationLevel.Function, "POST", Route = "user")] HttpRequestData req, FunctionContext executionContext)
+		{
+			// Parse input
+			string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+			User user = JsonConvert.DeserializeObject<User>(requestBody);
+
+			user.userId += 100;
+
+			// Generate output
+			HttpResponseData response = req.CreateResponse(HttpStatusCode.OK);
+
+			await response.WriteAsJsonAsync(user);
+
+			return response;
+		}
+
+		[Function(nameof(UserHttpTriggers.UpdateUser))]
+		[OpenApiOperation(operationId: "updateUser", tags: new[] { "user" }, Summary = "Update an existing user", Description = "This updates an existing user.", Visibility = OpenApiVisibilityType.Important)]
+		//[OpenApiSecurity("petstore_auth", SecuritySchemeType.Http, In = OpenApiSecurityLocationType.Header, Scheme = OpenApiSecuritySchemeType.Bearer, BearerFormat = "JWT")]
+		[OpenApiRequestBody(contentType: "application/json", bodyType: typeof(User), Required = true, Description = "User object that needs to be updated")]
+		[OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(User), Summary = "User details updated", Description = "User details updated", Example = typeof(DummyUserExample))]
+		[OpenApiResponseWithoutBody(statusCode: HttpStatusCode.BadRequest, Summary = "Invalid ID supplied", Description = "Invalid ID supplied")]
+		[OpenApiResponseWithoutBody(statusCode: HttpStatusCode.NotFound, Summary = "User not found", Description = "User not found")]
+		[OpenApiResponseWithoutBody(statusCode: HttpStatusCode.MethodNotAllowed, Summary = "Validation exception", Description = "Validation exception")]
+		public async Task<HttpResponseData> UpdateUser([HttpTrigger(AuthorizationLevel.Function, "PUT", Route = "user")] HttpRequestData req, FunctionContext executionContext)
+		{
+			// Parse input
+			string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+			User user = JsonConvert.DeserializeObject<User>(requestBody);
+
+			// Generate output
+			HttpResponseData response = req.CreateResponse(HttpStatusCode.OK);
+
+			await response.WriteAsJsonAsync(user);
+
+			return response;
+		}
+	}
+}
---
+++ Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Abstractions;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Resolvers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace KiCoKalender.Models
+{
+    [OpenApiExample(typeof(DummyUserExample))]
+    public class User
+    {
+        [OpenApiProperty(Description = "Gets or sets the parent ID.")]
+        public long? userId { get; set; }
+
+        [OpenApiProperty(Description = "Gets or sets the name.")]
+        [JsonRequired]
+        public string Name { get; set; }
+
+        [OpenApiProperty(Description = "Gets or sets the Role for the user.")]
+        [JsonRequired]
+        public Role role { get; set; }
+    }
+
+    public class DummyUserExample : OpenApiExample<User>
+    {
+        public override IOpenApiExample<User> Build(NamingStrategy NamingStrategy = null)
+        {
+            Examples.Add(OpenApiExampleResolver.Resolve("Dirk", "This is Dirk's summary",new User() { userId = 33, Name = "Dirk", role = Role.Parent}, NamingStrategy));
+            Examples.Add(OpenApiExampleResolver.Resolve("Jan", "This is Jan's summary", new User() { userId = 34, Name = "Jan", role = Role.Parent }, NamingStrategy));
+    
+            return this;
+        }
+    }
+}
