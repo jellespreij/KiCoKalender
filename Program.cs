8eae1eeafeec3c8eb6113a4e406fd6ce2dae75f7
Kim <565459@student.inholland.nl>
2021-09-30 20:55:55 +0200

    test

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using Interfaces;
+using Repository;
+using Services;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Extensions.OpenApi;
+using Microsoft.Azure.Functions.Worker.Extensions.OpenApi.Extensions;
+using Microsoft.Azure.Functions.Worker.Extensions.OpenApi.Functions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Security;
+using Microsoft.AspNetCore.Hosting;
+
+namespace KiCoKalender.Startup {
+	public class Program {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
+
+
